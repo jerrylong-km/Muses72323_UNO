38675431c8abc30cdbff3c3ff917bf97521cb894
jerrylong-km <53472123+jerrylong-km@users.noreply.github.com>
2024-02-05 23:19:23 +0800

    Add files via upload

--- Muses72323_on_ESP.ino
+++ Muses72323_on_ESP.ino
@@ -37,6 +37,15 @@ const uint8_t musesChipAddress = 0;
 #define MUSES_MOSI 23  // MOSI          (MOSI) 
 #define MUSES_CS    5  // Chip Select   (SS)
 
+//音量旋转编码器参数
+#define VOL_ENC_A 6   //接ESP32的GPIO6
+#define VOL_ENC_B 13  //接ESP32的GPIO13
+#define MaxVolume 100
+#define MinVolume 0
+int vol_counter = 0;
+int32_t VolumeVal = 0;
+char VolumeText[16];
+
 // define zcenb and csb pins for the MUSES72323
 const int zcenPin = 27;  // zero crossing enable, active low
 //const int csbPin = MUSES_CS;  // chip select, active low
@@ -58,9 +67,45 @@ const uint8_t musesRegMask   = 0x07<<4; // 寄存器地址掩码（1110000）
 const uint8_t musesZcBit     = 0x01<<5; // set bit 5 LOW in the data word and set zcenPin LOW to enable zero crossing detection
 const uint8_t musesLRAtt     = 0x01<<7; // set bit 7 HIGH to link right channel attenuation to left channel control register
 
+//音量旋转编码器对应的中断例程
+void IRAM_ATTR read_vol_encoder() {
+  // Encoder interrupt routine for both pins. Updates vol_counter
+  // if they are valid and have rotated a full indent
+ 
+  static uint8_t old_AB = 3;  // Lookup table index
+  static int8_t encval = 0;   // Encoder value  
+  static const int8_t enc_states[]  = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0}; // Lookup table
+
+  old_AB <<=2;  // Remember previous state
+
+  if (digitalRead(VOL_ENC_A)) old_AB |= 0x02; // Add current state of pin A
+  if (digitalRead(VOL_ENC_B)) old_AB |= 0x01; // Add current state of pin B
+  
+  encval += enc_states[( old_AB & 0x0f )];
+
+  // Update vol_counter if encoder has rotated a full indent, that is at least 4 steps
+  if( encval > 3 ) {        // Four steps forward
+    vol_counter++;              // Increase vol_counter
+    if (vol_counter > MaxVolume) vol_counter = 100;
+    encval = 0;
+  }
+  else if( encval < -3 ) {  // Four steps backwards
+   vol_counter--;               // Decrease vol_counter
+   if (vol_counter < MinVolume) vol_counter = 0;
+   encval = 0;
+  }
+}
+
 void setup() {
   Serial.begin(115200);
-  // set the CSB as an output   	it works without this! ?
+
+  //设置音量旋转编码器对应的GPIO，并关连相应中断
+  pinMode(VOL_ENC_A, INPUT_PULLUP);
+  pinMode(VOL_ENC_B, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(VOL_ENC_A), read_vol_encoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(VOL_ENC_B), read_vol_encoder, CHANGE);
+
+  // set the CSB as an output     it works without this! ?
   pinMode(MUSES_CS, OUTPUT);
   digitalWrite(MUSES_CS, HIGH);  
   // set the ZCEN pin as an output and activate it   	it works without this! ?
@@ -70,14 +115,33 @@ void setup() {
   SPI.begin();
   // MUSES72323 SPI interface is rated at 250kHz maximum
   SPI.beginTransaction(SPISettings(250000, MSBFIRST, SPI_MODE0));
-  // on reset, MUSES72320 is muted, so we set the default volume
+  // on reset, MUSES72323 is muted, so we set the default volume
   musesWriteVolume(defaultVolume, defaultVolume);
 }
 
 void loop()
 {
-  delay(3000); 
+  static int last_vol_counter = 0;//音量旋转编码器计数状态重置
+  // 音量发生变化
+   if(vol_counter != last_vol_counter)
+   {
+       Serial.print("vol_counter = ");
+       Serial.println(vol_counter);
+
+/*       //改变显示屏的音量显示数字
+       VolumeVal = vol_counter;
+       itoa(VolumeVal, VolumeText, 10);
+       lv_label_set_text(ui_Label2, VolumeText);
+       lv_slider_set_value(ui_Slider1, VolumeVal, LV_ANIM_ON);
+
+       //将变化后的音量值写入EEPROM
+       EEPROM.write(0, vol_counter);
+       EEPROM.commit();
+       last_vol_counter = vol_counter;
+*/
+   }
   musesWriteVolume(-80, -80);
+  delay(3000); 
 }
 
 uint8_t volumeToAtt(long volume) {
@@ -123,4 +187,3 @@ void musesWriteRaw(uint8_t address, uint8_t data) {
   delayMicroseconds(1);   // this can be improved
   digitalWrite(MUSES_CS, HIGH);
 }
-
